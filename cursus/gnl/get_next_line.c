6706fee55a06e998109495578596711e71bff326
Gael GOSSE <ggosse@e1r12p6.clusters.42paris.fr>
2022-07-24 19:10:47 +0200

    end_of_gnl

--- cursus/gnl/get_next_line.c
+++ cursus/gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ggosse <ggosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 22:10:39 by gael              #+#    #+#             */
-/*   Updated: 2022/07/21 16:03:29 by ggosse           ###   ########.fr       */
+/*   Updated: 2022/07/24 18:14:14 by ggosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buf[BUF_SIZE + 1];
+	static char	buf[BUFFER_SIZE + 1];
 	char		*line;
 	int			ret;
 
-	if (fd < 0 || fd > 1024 || BUF_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	line = NULL;
 	if (ft_strlen(buf) != 0)
 		line = ft_strdup(buf);
 	while (!(ft_strchr(buf, '\n')))
 	{
-		ret = read(fd, buf, BUF_SIZE);
-		if (ret < 0)	
-			return (NULL);
+		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret < 0)
+			return (free(line), NULL);
 		buf[ret] = '\0';
 		if (ret == 0)
 			return (ft_strjoin(line, buf));
@@ -41,72 +41,3 @@ char	*get_next_line(int fd)
 		return (free(line), NULL);
 	return (ft_save(line, buf), line); 
 }
-
-/*
-int main()
-{
-	// printf(back_green"\n----- ----- START ----- -----\n"reset);
-
-
-
-
-	// printf("\n\n"back_green" --- a1.txt --- "reset"\n");
-	// int	fd = open("a1.txt", O_RDONLY);
-	// if (fd == -1)
-	// 	return (printf("open err\n"), 0);
-	// printf(bold_cyan"%s"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s"reset"",get_next_line(fd));
-	// printf(back_red"\n END "reset);
-	// if (fd == -1)
-	// 	return (printf("open err\n"), 0);
-
-
-
-
-	// printf("\n\n"back_green" --- a2.txt --- "reset"\n");
-	// fd = open("a2.txt", O_RDONLY);
-	// if (fd == -1)
-	// 	return (printf("open err\n"), 0);
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(back_red" END "reset);
-	// if (fd == -1)
-	// 	return (printf("open err\n"), 0);
-
-
-
-
-	// printf("\n\n"back_green" --- a3.txt --- "reset"\n");
-	int	fd = open("a3.txt", O_RDONLY);
-	if (fd == -1)
-		return (printf("open err\n"), 0);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	get_next_line(fd);
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	// printf(back_red" END "reset);
-	if (fd == -1)
-		return (printf("open err\n"), 0);
-
-
-	// int	fd = open("a4.txt", O_RDONLY);
-	// if (fd == -1)
-	// 	return (printf("open err\n"), 0);
-	// printf("\n\n"back_green" --- a4.txt --- "reset"\n");
-	// printf(bold_cyan"%s\n"reset"",get_next_line(fd));
-	
-	// printf("\n");
-}
-*/
--- cursus/gnl/get_next_line.h
+++ cursus/gnl/get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: ggosse <ggosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 15:01:22 by ggosse            #+#    #+#             */
-/*   Updated: 2022/07/21 15:08:21 by ggosse           ###   ########.fr       */
+/*   Updated: 2022/07/24 18:14:44 by ggosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GNL_H
-# define GNL_H
+#ifndef FT_GNL_H
+# define FT_GNL_H
 
-#define BUF_SIZE 15
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -21,7 +20,6 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-// # include "color.h"
 
 char			*get_next_line(int fd);
 char			*ft_strdup(const char *src);
