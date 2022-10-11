7314a968255e112ac42dcf1a52feb151a7ae7c9e
Gael GOSSE <ggosse@e3r5p14.clusters.42paris.fr>
2022-10-11 19:19:49 +0200

    start_push_swap

---
+++ cursus/push_swap/ft_push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_swap.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggosse <ggosse@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 15:55:04 by ggosse            #+#    #+#             */
+/*   Updated: 2022/10/11 16:10:42 by ggosse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <ctype.h>
+# include <string.h>
+# include <stddef.h>
+# include "libft/libft.h"
+
+typedef struct p_list
+{
+	int				*nbr;
+	struct	p_list	*next;
+}					r_list;
+
+#endif
