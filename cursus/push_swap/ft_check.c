877deaa538a214641475179d46b7c884d2f4c685
Gael GOSSE <ggosse@e3r9p10.clusters.42paris.fr>
2022-11-30 19:19:17 +0100

    tester_push_swap

--- cursus/push_swap/ft_check.c
+++ cursus/push_swap/ft_check.c
@@ -6,12 +6,11 @@
 /*   By: ggosse <ggosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 18:05:55 by ggosse            #+#    #+#             */
-/*   Updated: 2022/11/30 19:04:35 by ggosse           ###   ########.fr       */
+/*   Updated: 2022/11/30 19:08:43 by ggosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
-
 
 void	ft_check_args(char **argv)
 {
