0969a5a8f383e7847f7155f65b1552de426dcb3a
Gael GOSSE <ggosse@e2r6p10.clusters.42paris.fr>
2023-01-11 20:24:53 +0100

    multi_pipe

--- cursus/pipex_2/tester/long_envi.sh
+++ cursus/pipex_2/tester/long_envi.sh
@@ -12,20 +12,11 @@ big_bad=$BACK_RED" Error "$RST
 
 ##### TEST TO DO #####
 gcc -Wall -Wextra -Werror -g3 */*.c *.c */*.h *.h -o pipex
-# rm *.gch
-# rm */*.gch
+# rm -f .gch
+# rm -f /*.gch
 
 echo -e $BOLD_WHITE" ----- ----- ----- ----- "$RST" $BACK_YELLOW $(date +"%H:%M:%S") $RST"
-echo -e $RED"./pipex infile \"ls\" \"/usr/bin/wc -l\" \"grep -v 1\" \"dslhkc\" outfile"$RST"\n"
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./pipex infile "ls" "/usr/bin/wc -l" "grep -v 1" "dslhkc" outfile
+echo -e $CYAN"env -i ./pipex infile \"ls\" \"/usr/bin/wc -l\" \"dslhkc\" \"grep -v 1\" outfile"$RST"\n"
+# valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes 
+env -i ./pipex infile "ls" "/usr/bin/wc -l" "dslhkc" "grep -v 1" outfile
 
-
-# mv little.sh little_env_i.sh
-# mv long.sh long_env_i.sh
-# mv mini.sh mini_env_i.sh
-# mv tester.sh tester_env_i.sh
-
-cp tester/little.sh tester/little_envi.sh
-cp tester/long.sh tester/long_envi.sh
-cp tester/mini.sh tester/mini_envi.sh
-cp tester/tester.sh tester/tester_envi.sh
--- cursus/pipex_2/tester/mini.sh
+++ cursus/pipex_2/tester/mini.sh
@@ -13,10 +13,10 @@ big_bad=$BACK_RED" Error "$RST
 
 ##### TEST TO DO #####
 gcc -Wall -Wextra -Werror -g3 */*.c *.c */*.h *.h -o pipex
-rm *.gch */*.gch
+rm -f .gch */*.gch
 
-echo -e $RED" ----- ----- ----- ----- "$RST" $BACK_YELLOW $(date +"%H:%M:%S") $RST"
-echo -e $RED"./pipex infile \"ls\" \"/usr/bin/grep c\" outfile"$RST"\n"
+echo -e $CYAN" ----- ----- ----- ----- "$RST" $BACK_YELLOW $(date +"%H:%M:%S") $RST"
+echo -e $CYAN"./pipex infile \"ls\" \"/usr/bin/grep c\" outfile"$RST"\n"
 # valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes  --leak-check=full --show-leak-kinds=all --track-origins=yes 
 ./pipex infile "ls" "/usr/bin/grep c" outfile
 
