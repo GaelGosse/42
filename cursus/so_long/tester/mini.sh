0a6c4c972fd89064db1e54fa330e4efaa2b1eebb
Gael GOSSE <ggosse@e2r4p21.clusters.42paris.fr>
2023-01-17 18:54:50 +0100

    redump

--- cursus/so_long/tester/mini.sh
+++ cursus/so_long/tester/mini.sh
@@ -6,11 +6,11 @@ rm -f *.gch */*.gch
 if [[ $# == 1 ]]
 then
 	echo -e $CYAN" ----- $1 ----- "$RST
-	gcc -Wall -Wextra -Werror *.c */*.c *.h */*.h -g -o so_long && ./so_long $1
+	gcc -Wall -Wextra -Werror *.c libft/*.c *.h libft/*.h -g -o so_long && ./so_long $1
 elif [[ $# == 0 ]]
 then
 	echo -e $CYAN" ----- map_1 ----- "$RST
-	gcc -Wall -Wextra -Werror *.c */*.c *.h */*.h -g -o so_long && ./so_long maps/map_1.ber
+	gcc -Wall -Wextra -Werror *.c libft/*.c *.h libft/*.h -g -o so_long && ./so_long maps/map_1.ber
 else
 	echo "wrong arg"
 fi
