1fd67167f8e3a8c52745af32b9b142dc5538728e
GaelGosse <gael.gosse@yahoo.fr>
2023-02-11 19:49:26 +0100

    philo:start_enum

--- cursus/philo/ft_philo.c
+++ cursus/philo/ft_philo.c
@@ -6,18 +6,22 @@
 /*   By: ggosse <ggosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:51:05 by ggosse            #+#    #+#             */
-/*   Updated: 2023/02/09 15:53:31 by ggosse           ###   ########.fr       */
+/*   Updated: 2023/02/11 19:49:08 by ggosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo.h"
 
+enum e_args{nbr_of_philo=NULL,die_time=NULL,eat_time=NULL,sleep_time=NULL,cycles=NULL}
+
+
 int	main(int argc, char **argv){
-	if (argc == 5 || argc == 6)
-	{
-		number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
-		
-	}
+	enum e_args 
+	
+	// if (argc == 5 || argc == 6)
+	// {
+
+	// }
 	(void)argc;
 	(void)argv;
 }
--- cursus/philo/ft_philo.h
+++ cursus/philo/ft_philo.h
@@ -1,37 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_phio.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ggosse <ggosse@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 15:44:42 by ggosse            #+#    #+#             */
-/*   Updated: 2023/02/09 15:50:47 by ggosse           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef FT_PHILO_H
+# define FT_PHILO_H
 
-
-#ifndef FT_SO_LONG_H
-# define FT_SO_LONG_H
-
-# include <stdio.h>
+# include <dirent.h>
 # include <errno.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <sys/wait.h>
-# include <unistd.h>
-# include <string.h>
 # include <fcntl.h>
-
-# define SUCCESS 1
-# define FAIL -1
-
-typedef struct s_philos
-{
-}			t_philos;
-
-/********************************** PARSING ***********************************/
+# include <limits.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <termios.h>
+# include <unistd.h>
 
 
 #endif
