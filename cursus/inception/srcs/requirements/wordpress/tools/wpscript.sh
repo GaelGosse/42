1ed3e2264de4615181c16bf6602d14f8549646a2
GaelGosse <gael.gosse@yahoo.fr>
2024-05-08 10:59:37 +0200

    inception_correction

--- cursus/inception/srcs/requirements/wordpress/tools/wpscript.sh
+++ cursus/inception/srcs/requirements/wordpress/tools/wpscript.sh
@@ -19,23 +19,23 @@ wp core install	--allow-root \
 
 wp user create		--allow-root \
 			${USER1_LOGIN} ${USER1_MAIL} \
-			--role=author \
 			--user_pass=${USER1_PASS} ;
 
 wp cache flush --allow-root
 
+			# --role=author \
 # it provides an easy-to-use interface for creating custom contact forms and managing submissions, as well as supporting various anti-spam techniques
-wp plugin install contact-form-7 --activate
+# wp plugin install contact-form-7 --activate
 
-# set the site language to English
-wp language core install en_US --activate
+# # set the site language to English
+# wp language core install en_US --activate
 
-# remove default themes and plugins
-wp theme delete twentynineteen twentytwenty
-wp plugin delete hello
+# # remove default themes and plugins
+# wp theme delete twentynineteen twentytwenty
+# wp plugin delete hello
 
-# set the permalink structure
-wp rewrite structure '/%postname%/'
+# # set the permalink structure
+# wp rewrite structure '/%postname%/'
 
 fi
 
@@ -43,5 +43,5 @@ if [ ! -d /run/php ]; then
 	mkdir /run/php;
 fi
 
-# start the PHP FastCGI Process Manager (FPM) for PHP version 7.3 in the foreground
-exec /usr/sbin/php-fpm7.3 -F -R
+# start the PHP FastCGI Process Manager (FPM) for PHP version 7.4 in the foreground
+exec /usr/sbin/php-fpm7.4 -F -R
