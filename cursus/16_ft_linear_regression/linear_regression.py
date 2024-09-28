02350287abaaeaa18646daa5862ca00753a0f871
GaelGosse <gael.gosse@yahoo.fr>
2024-09-28 22:02:32 +0200

    16_normalisation

--- cursus/16_ft_linear_regression/linear_regression.py
+++ cursus/16_ft_linear_regression/linear_regression.py
@@ -19,17 +19,24 @@ MAGENTA = "\033[95m"
 CYAN = "\033[96m"
 RESET = "\033[0m"
 
+
 # ----- ----- READ CSV DATA ----- -----
-data = pds.read_csv('data.csv')
+data = pds.read_csv('invert.csv')
 x_label = data.columns[0]
 y_label = data.columns[1]
 
 
 # ----- ----- TERMINAL GRAPH ----- -----
-km_min = math.floor(data.min()['km'])
-c0_max = math.ceil(data.max()['km'])
-price_min = math.floor(data.min()['price'])
-price_max = math.ceil(data.max()['price'])
+c0_min = math.floor(data.min()[data.columns[0]]);
+c0_max = math.ceil(data.max()[data.columns[0]]);
+c1_min = math.floor(data.min()[data.columns[1]]);
+c1_max = math.ceil(data.max()[data.columns[1]]);
+
+
+# ----- ----- NORMALISATION ----- -----
+norm_data = pds.DataFrame({data.columns[0]: data[data.columns[0]], data.columns[1]: data[data.columns[1]]})
+norm_data[data.columns[0]] = norm_data[data.columns[0]].apply(lambda x: x / c0_max)
+norm_data[data.columns[1]] = norm_data[data.columns[1]].apply(lambda x: x / c1_max)
 
 
 # ----- ----- LINEAR FUNCTION ----- -----
@@ -44,7 +51,7 @@ def perpendicular(x, a, b):
 
 # ----- ----- CREATE GRAPH ----- -----
 fig, ax = plt.subplots()
-sc = ax.scatter(data[x_label], data[y_label], label='cars', color='red')
+sc = ax.scatter(norm_data[x_label], norm_data[y_label], label='cars', color='red')
 # print(sc.get_offsets()[0][1])
 line, = ax.plot([], [], label='correlation km & price')
 
@@ -68,16 +75,9 @@ def check_dist_dots(a, b):
 		act_dist.append(dist)
 
 
-# ----- ----- NORMALISATION ----- -----
-# print("@", data.to_numpy())
-norm_data = pds.DataFrame({data.columns[0]: data[data.columns[0]], data.columns[1]: data[data.columns[1]]})
-norm_data['km'] = norm_data['km'].apply(lambda x: x / c0_max)
-norm_data['km'] = norm_data['km'].apply(lambda x: x / c0_max)
-print(f"{norm_data}\n{data}")
-
 # ----- ----- GRAPH SETTINGS ----- -----
-ax.set_xlim([0, 1])
-ax.set_ylim([0, 1])
+ax.set_xlim([-0.1, 1.1])
+ax.set_ylim([-0.1, 1.1])
 
 ax.set_xlabel(x_label)
 ax.set_ylabel(y_label)
@@ -91,18 +91,44 @@ def clearly(number):
 
 
 # ----- ----- ANIMATION ----- -----
+
+def	mean_half_array(arr):
+	if len(arr) == 0:
+		return (None, None)
+
+	mid_index = len(arr) // 2
+	first_half = arr[:mid_index]
+	second_half = arr[mid_index:]
+
+	first_mean = sum(first_half) / len(first_half) if not first_half.empty else None
+	second_mean = sum(second_half) / len(second_half) if not second_half.empty else None
+
+	if first_mean is None or second_mean is None:
+		return None
+
+	if (first_mean > second_mean):
+		return 1
+	else:
+		return 0
+
 final_a = 0.01
-final_b = (price_max - price_min) / 2 + price_min
+final_b = (c1_max - c1_min) / 2 + c1_min
 
 step = 0
 param_a = -90
+step_a = 1
 min_a = np.sin(param_a / 10000)
 
+final_b = mean_half_array(norm_data[data.columns[1]])
+if (final_b == 1):
+	step_a = -1
+else:
+	step_a = 1
 def update(frame):
-	global step, param_a, min_a
-	final_a = np.sin(param_a / 10000)
-	final_b = (price_max - price_min) / 2 + price_min
+	global step, param_a, min_a, final_a, final_b, step_a
+	final_a = np.sin(param_a / 1000)
 
+	print('final_b:', final_b);
 	# final_a = -0.03056
 	# final_b = 9000
 
@@ -112,13 +138,13 @@ def update(frame):
 	sum_prev_dist = sum(prev_dist)
 	if (sum_prev_dist != 0 and sum_act_dist < sum_prev_dist):
 		min_a = param_a
-		print(GREEN, f"sum_act_dist: {clearly(int(sum_act_dist))}", RESET)
+		print(GREEN, f"sum_act_dist: {clearly(sum_act_dist)}", RESET)
 	if (sum_prev_dist != 0 and sum_act_dist > sum_prev_dist):
-		print(RED, f"sum_act_dist: {clearly(int(sum_act_dist))}", RESET)
+		print(RED, f"sum_act_dist: {clearly(sum_act_dist)}", RESET)
 		ani.event_source.stop()
 
 
-	param_a += 1
+	param_a += step_a
 
 	print(f"n°{step}    main(x)= {final_a}\t* x + {final_b}")
 	x = np.linspace(0, c0_max, c0_max)
@@ -129,7 +155,7 @@ def update(frame):
 	return line,
 
 # ani = FuncAnimation(fig, update, frames=np.arange(-90, 90), fargs=(ani,), blit=True, interval=1000, repeat=False)
-# ani = FuncAnimation(fig, update, frames=np.arange(-90, 90), blit=True, interval=10, repeat=False)
+ani = FuncAnimation(fig, update, frames=np.arange(-90, 90), blit=True, interval=10, repeat=True)
 
 
 
